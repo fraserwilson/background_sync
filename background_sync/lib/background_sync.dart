7f5f2e04a2d4a5c34fb220905e7c9a9f8f565250
Fraser Wilson <FraserW@discovery.co.za>
2024-08-01 12:41:23 +0200

    Removing broken project

--- background_sync/lib/background_sync.dart
+++
@@ -1,8 +0,0 @@
-
-import 'background_sync_platform_interface.dart';
-
-class BackgroundSync {
-  Future<String?> getPlatformVersion() {
-    return BackgroundSyncPlatform.instance.getPlatformVersion();
-  }
-}
